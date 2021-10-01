dc294501efe2fc7afa47b7afe4bc8513621a9f30
kazuryu <41988990+Kazuryu0907@users.noreply.github.com>
2021-10-01 20:22:02 +0900

    u

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 token =  "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"
 from github import Github
-
+import base64
 g = Github(token)
-for repo in g.get_user().get_repos(type="owner"):
-    print(repo.name)
+repo = g.get_repo("Kazuryu0907/ANKAChan")
+file = "version.txt"
+contents = repo.get_contents(file)
+content = base64.b64decode(contents.content)
+print(content)
